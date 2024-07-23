b5965020a0f3b7201a099a8b5d5b36f3cd6ae7ec
zlacore <zlacore12@gmail.com>
2024-07-23 05:16:42 -0500

    added functions to js file

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,28 @@
-const topic = "HTML";
-
-/* if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log('Please try again!');
-}
-*/
-
 const topics = ["HTML", "CSS", "Git", "JavaScript"];
-for (let x = 0; x < topics.length; x++) {
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+  for (let x = 0; x < topics.length; x++) { 
     console.log(topics[x]);
+    }
 }
+
+function selectTopics () {
+  if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+}
+
+console.log("Here are the topics we learned through prework:")
+listTopics() 
+
+console.log("Which subject should we study first?")
+selectTopics()
